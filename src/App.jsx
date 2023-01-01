42f02ed87427add766c101cce708c6e473d8f310
Marvin M. Pacis <marvedcodes@gmail.com>
2023-01-01 15:35:09 +0800

    Create ThemeContext based on LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import styled from 'styled-components';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 const App = () => {
-  return <div>React</div>;
+  return (
+    <ThemeContextProvider>
+      <Container></Container>
+    </ThemeContextProvider>
+  );
 };
+
+const Container = styled.main`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: ${props => props.theme.colors.background};
+  padding: 3.1rem 2.4rem;
+
+  @media (min-width: 768px) {
+    padding: 3.1rem 7rem;
+  }
+`;
 
 export default App;
---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+export const ThemeContext = createContext(null);
+
+const ThemeContextProvider = () => {
+  const [lightMode, setLightMode] = useState(false);
+
+  const darkTheme = {
+    colors: {
+      background: '#141D2F',
+      themeBtn: '#fff',
+      card: '#1E2A47',
+      textNorm: '#fff',
+      textBolded: '#FFF',
+    },
+  };
+  const lightTheme = {
+    colors: {
+      background: '#F6F8FF',
+      themeBtn: '#4B6A9B',
+      card: '#FEFEFE',
+      textNorm: '#697C9A',
+      textBolded: '#2B3442',
+    },
+  };
+
+  const changeTheme = () => {
+    setLightMode(prevState => !prevState);
+  };
+
+  useEffect(() => {
+    localStorage.getItem('theme') === 'light' && setLightMode(true);
+  }, []);
+
+  useEffect(() => {
+    const mode = lightMode ? 'light' : 'dark';
+
+    localStorage.setItem('theme', mode);
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ changeTheme, lightTheme, darkTheme }}>
+      <ThemeProvider theme={lightMode ? lightTheme : darkTheme}></ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContextProvider;
